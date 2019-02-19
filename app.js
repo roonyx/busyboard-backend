c232c0051f84553f803fb6555ae136823ec9bf2f
Mark <iglad96@gmail.com>
2019-02-19 12:41:37 +0300

    Add port from env, move mock data to fixtures folder

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const data = require('./mockdata');
+const data = require('./fixtures/mockdata');
+
+const port = process.env.PORT || 3000;
+
 app.get('/api/worklogs', function (request, response) {
   response.send(data);
 });
-app.listen(3000);
+app.listen(port);
