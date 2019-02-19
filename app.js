bed3bfe6236ac174b8e79c5a2f7c271fd5443e29
Mark <iglad96@gmail.com>
2019-02-19 12:01:16 +0300

    Add mockData, add express, build server

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// busy-board-backend
+const express = require('express');
+const app = express();
+const data = require('./mockdata');
+app.get('/worklogs', function (request, response) {
+  response.send(data);
+});
+app.listen(3000);
